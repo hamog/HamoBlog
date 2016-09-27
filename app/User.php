cf7d2cc99500f3eb2729a959ce40374b6faaeb6f
hashem mo <hashemm364@gmail.com>
2016-09-27 20:03:40 +0330

    Generate simple bootsrap pagination for home page

--- app/User.php
+++ app/User.php
@@ -44,6 +44,11 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = strtolower($name);
+    }
+
     /**
      * Get admin user.
      *
